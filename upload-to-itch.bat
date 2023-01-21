8a220641f89db7c067639b82e81dd8fc18387d97
Malcolm Nixon <Malcolm.nixon@gmail.com>
2023-01-21 00:54:47 -0500

    Updated to publish webxr

--- upload-to-itch.bat
+++ upload-to-itch.bat
@@ -1,9 +1,11 @@
 SET CURRENT_DIR=%~dp0
-SET APK=%CURRENT_DIR%/build/VertigoMaze.apk
+SET APK=%CURRENT_DIR%/build/quest/VertigoMaze.apk
 SET WINDOWS=%CURRENT_DIR%/build/windows/
+SET WEBXR=%CURRENT_DIR%/build/webxr/
 
 echo Uploading to Itch...
 butler -v push "%APK%" "malcolmnixon/vertigo-maze:android"
 butler -v push "%WINDOWS%" "malcolmnixon/vertigo-maze:windows"
+butler -v push "%WEBXR%" "malcolmnixon/vertigo-maze:webxr"
 
 echo Done.
--- upload-to-itch.sh
+++ upload-to-itch.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 CURRENT_DIR=$(dirname "$(readlink -f "$0")")
-APK="$CURRENT_DIR/build/VertigoMaze.apk"
+APK="$CURRENT_DIR/build/quest/VertigoMaze.apk"
 WINDOWS="$CURRENT_DIR/build/windows/"
+WEBXR="$CURRENT_DIR/build/webxr/"
 
 echo Uploading to Itch...
 butler -v push "$APK" "malcolmnixon/vertigo-maze:android"
 butler -v push "$WINDOWS" "malcolmnixon/vertigo-maze:windows"
+butler -v push "$WEBXR" "malcolmnixon/vertigo-maze:webxr"
 
 echo Done.
